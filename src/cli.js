23b45ebaa55d6351e2ef210e19e632045fe35397
Tom Milligan <tommilligan@users.noreply.github.com>
2018-12-28 14:13:25 +0000

    updated warnings and cli input

--- src/cli.js
+++ src/cli.js
@@ -1,47 +1,54 @@
 #!/usr/bin/env node
 
-const chalk = require("chalk");
-const program = require("commander");
+const chalk = require('chalk');
+const program = require('commander');
 
-const colorBlindChecker = require("./index");
+const colorBlindChecker = require('./index');
 
 const log = console.log;
 
 program
-    .version("0.1.0")
-    .option("-0, --color0 <color>", "color 0")
-    .option("-1, --color1 <color>", "color 1")
-    .option("--text", "Treat colors as text and background values")
-    .parse(process.argv);
+  .version('0.1.0')
+  .usage('[options] <color0> <color1>')
+  .option('--text', 'Treat colors as text and background values')
+  .parse(process.argv);
 
-result = new colorBlindChecker.Checker(program.color0, program.color1).check();
+result = new colorBlindChecker.Checker(
+  program.args[0],
+  program.args[1],
+).check();
 
 //console.log(JSON.stringify(result, null, 3));
 
 function blockOf(color) {
-    return chalk.hex(color)("██");
+  return chalk.bgHex(color)('  ');
 }
 
-function warn(warning) {
-    log(`${chalk.bold("Warning: ")} ${warning}`);
+function warnFilter(filterName, warning) {
+  log(`${chalk.bold(`Warning (${filterName}):`)} ${warning}`);
 }
 
 for (const filter of Object.keys(result.filters)) {
-    filterResult = result.filters[filter];
+  const warn = warning => warnFilter(filter, warning);
+  const filterResult = result.filters[filter];
 
-    log(
-        `Checked ${filter}: ${blockOf(filterResult.colors[0])}${blockOf(
-            filterResult.colors[1]
-        )}`
+  log(
+    `Checked ${blockOf(filterResult.colors[0])}${blockOf(
+      filterResult.colors[1],
+    )} (${filter})`,
+  );
+
+  if (program.text && filterResult.warnings.lowContrast) {
+    warn(
+      `Too low contrast for text (${chalk
+        .hex(filterResult.colors[0])
+        .bgHex(filterResult.colors[1])('sample')})`,
     );
-
-    if (program.text && filterResult.warnings.lowContrast) {
-        warn("Too low contrast for text");
-    }
-    if (filterResult.warnings.informationLoss) {
-        warn("Colors are much closer than with full color vision");
-    }
-    if (filterResult.warnings.indistinguishable) {
-        warn("Colors are functionally indistinguishable");
-    }
+  }
+  if (filterResult.warnings.informationLoss) {
+    warn('Colors are much closer than with full color vision');
+  }
+  if (filterResult.warnings.indistinguishable) {
+    warn('Colors are functionally indistinguishable');
+  }
 }
